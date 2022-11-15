5e175d9b76f0f9a177d66f40cd7b603eb7583786
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-11-15 15:53:33 +0100

    codice base e creazione funzione

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Palidroma
+
+
+// Chiedere all’utente di inserire una parola
+let parolaUtente = prompt("Inserisci una parola");
+
+const risultato = palindroma
+
+console.log(risultato)
+
+
+// Creare una funzione per capire se la parola inserita è palindroma
+function palindroma(){
+    if(parolaUtente === parolaUtente.reverse()){
+
+        output = "palindroma"
+    }else{
+        output = "non palindorma"
+    }
+
+    return risultato;
+}
